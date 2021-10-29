cc7acb6409ed28be1db5699baa7d1d5ac5729fa7
Chao Lin <clin135@binghamton.edu>
2021-10-29 16:22:44 -0400

    commit 1

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #import your controller
-
+import game
 def main():
+	pygame.init()
+	team = {"lead": "Ira", "backend": "Chao", "frontend": "Carl"}
+	print("Software Lead is:", team["lead"])
+	print("Backend is:", team["backend"])
+	print("Frontend is:" , team["frontend"])
+
     #Create an instance on your controller object
     #Call your mainloop
     

7248092769c0eaa982df6a30a5f01a794c90527b
Ira Cheng <icheng4@binghamton.edu>
2021-11-05 12:46:27 -0400

    Commit 1.1

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #import your controller
-import game
+
+import pygame
+
 def main():
 	pygame.init()
-	team = {"lead": "Ira", "backend": "Chao", "frontend": "Carl"}
+	team = {"lead": "Ira Cheng", "backend": "Chao Lin", "frontend": "Carl Huang"}
 	print("Software Lead is:", team["lead"])
 	print("Backend is:", team["backend"])
 	print("Frontend is:" , team["frontend"])
 
     #Create an instance on your controller object
+
     #Call your mainloop
-    
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 2 LINES OF CODE ######
+
 main()
+
+    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 2 LINES OF CODE ######

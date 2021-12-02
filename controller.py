a8847a8ac36f20db8ba85637ea89029555fd9b7e
Ira Cheng <icheng4@binghamton.edu>
2021-12-01 23:22:10 -0500

    Milestone V

--- controller.py
+++ controller.py
@@ -1,8 +1,17 @@
 import pygame
 from src import ship
 from src import monster 
+
 class Controller:
+
 	def __init__(self,width = 500,height=600):
+
+		"""
+		This method sets all of the necessary parameters for the game window and game objects and spawns in all of the necessary objects for the game. It takes in parameters for the size of the window that the game runs in, creates said window, spawns in the  
+		args: self which is the object created from the class. width (int) which is the width of the screen that the game is to be played on. height (int) which is the height of the screen that the game is to be played on. 
+		return: none
+		"""
+
 		pygame.init()
 		self.width = width
 		self.height = height
@@ -18,7 +27,15 @@ class Controller:
 		for i in range(5):
 			self.monster = monster.Monster()
 			self.all_sprite.add(self.monster)
+
 	def mainLoop(self):
+
+		"""																																																			
+		This method checks to see whether or not the gameplay is continuing or if the gameplay is over. It accomplishes such by checking if the state of the game is either “running.” If the game is in a “running” state the gameplay of the program is run. 
+		args: self which is the object that is created from the class. 
+		return: none
+		"""
+
 		pygame.init()
 		while self.running:
 			self.clock.tick(self.fps)
@@ -27,13 +44,9 @@ class Controller:
 					self.running = False 
 			self.screen.fill(self.white)
 			self.all_sprite.draw(self.screen)
-			
-
 
 			#screen update
 			pygame.display.update()
 			self.all_sprite.update()
-
-
 
 		pygame.quit()		
